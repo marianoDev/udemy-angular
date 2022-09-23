c949d6de7aa1d6aa2aa105ec1a0eac3170bb4957
mariano <mariano.a.rodriguez@accenture.com>
2022-09-23 10:02:46 -0300

    chore(dropdown): section-8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipe
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.
     RecipesItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    RecipesDetailComponent
+    RecipesDetailComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule
